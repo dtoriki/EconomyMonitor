b9bf5646c3d80c5e2fc84c763fd9ca626546a5b5
dtoriki <dev.toriki@gmail.com>
2022-04-14 00:22:43 +0500

    [WIP] Wpf app created.

---
+++ src/EconomyMonitor.App.WPF/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace EconomyMonitor.App.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow() => InitializeComponent();
+}
