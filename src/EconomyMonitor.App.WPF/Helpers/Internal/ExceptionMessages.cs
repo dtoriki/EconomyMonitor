9bc98515c16c47b194c0f4902d19faa022f2727b
dtoriki <dev.toriki@gmail.com>
2022-04-16 22:11:43 +0500

    [WIP] Squash libraries.

---
+++ src/EconomyMonitor.App.WPF/Helpers/Internal/ExceptionMessages.cs
@@ -0,0 +1,10 @@
+namespace EconomyMonitor.App.WPF.Helpers.Internal;
+
+internal static class ExceptionMessages
+{
+    public static string ARGUMENT_OUT_OF_RANGE_CANT_BE_LOWER => "The argument with name \"{0}\" can not be lower than \"{1}\".";
+    public static string ARGUMENT_OUT_OF_RANGE_CANT_BE_HIGHER => "The argument with name \"{0}\" can not be higher than \"{1}\".";
+    public static string INDEX_CANT_BE_LOWER_TAHN_ZERO => "The index of array cannot be lower than zero.";
+    public static string OBSCURE_EXCEPTION_THROWN => "Obscure exception thrown.";
+    public static string NEED_AT_LEAST_A_MESSAGE => "Need at least an exception message.";
+}
---
+++ src/EconomyMonitor.App.WPF/MVVM/Abstracts/ViewModelBase.cs
@@ -0,0 +1,56 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using EconomyMonitor.App.WPF.Helpers;
+
+namespace EconomyMonitor.App.WPF.MVVM.Abstracts;
+
+/// <summary>
+/// Base type of view model.
+/// </summary>
+/// <exception cref="ArgumentNullException"/>
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+    /// <inheritdoc/>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Creates a view model exemplar.
+    /// </summary>
+    protected ViewModelBase() { }
+
+    /// <summary>
+    /// Rises event of property changing.
+    /// </summary>
+    /// <param name="propertyName">Property's name.</param>
+    /// <exception cref="ArgumentNullException">Throws when <paramref name="propertyName"/> is <see langword="null"/>.</exception>
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    /// <summary>
+    /// Sets a <paramref name="value"/> to <paramref name="field"/> and notifies about changing.
+    /// </summary>
+    /// <typeparam name="T">Field's type.</typeparam>
+    /// <param name="field">Changing field.</param>
+    /// <param name="value">New field's value.</param>
+    /// <param name="propertyName">Notifiable property's name.</param>
+    /// <returns>
+    /// <see langword="true"/> if <paramref name="field"/> has chanched, otherwise - <see langword="false"/>.
+    /// </returns>
+    /// <remarks>
+    /// This method recursive safety. 
+    /// After the <paramref name="field"/> has changed, 
+    /// method rises <see cref="PropertyChanged"/> event if <paramref name="propertyName"/> not empty. 
+    /// </remarks>
+    protected virtual bool SetPropertyNotifiable<T>(ref T? field, T? value, [CallerMemberName] string propertyName = "")
+    {
+        void Notify()
+        {
+            if (!string.IsNullOrWhiteSpace(propertyName))
+            {
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        return ArgsHelper.Set(ref field, value, Notify);
+    }
+}
