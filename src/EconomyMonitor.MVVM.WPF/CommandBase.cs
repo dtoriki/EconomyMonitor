929f50756079bfb5e4e0a57373295c8a764e5b29
dtoriki <dev.toriki@gmail.com>
2022-04-16 15:43:17 +0500

    [WIP] Added ICommand implementations.
    Added:
    - CommandBase;
    - RelayCommand.

---
+++ src/EconomyMonitor.MVVM.WPF/CommandBase.cs
@@ -0,0 +1,33 @@
+using System.Windows.Input;
+
+namespace EconomyMonitor.MVVM.Common;
+
+/// <summary>
+/// Base <see cref="ICommand"/> implementation.
+/// </summary>
+/// <remarks>
+/// Delegates <see cref="CanExecuteChanged"/> event suscribing/unsubscribing to <see cref="CommandManager.RequerySuggested"/>.
+/// </remarks>
+public abstract class CommandBase : ICommand
+{
+    /// <inheritdoc/>
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    /// <summary>
+    /// Creates command exemplar.
+    /// </summary>
+    /// <remarks>
+    /// Implements <see cref="ICommand"/>. Delegates <see cref="CanExecuteChanged"/> event suscribing/unsubscribing to <see cref="CommandManager.RequerySuggested"/>.
+    /// </remarks>
+    protected CommandBase() { }
+
+    /// <inheritdoc/>
+    public abstract bool CanExecute(object? parameter);
+
+    /// <inheritdoc/>
+    public abstract void Execute(object? parameter);
+}
