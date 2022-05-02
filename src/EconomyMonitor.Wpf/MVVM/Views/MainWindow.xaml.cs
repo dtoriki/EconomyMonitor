8b63bedabf4817d47e96e53b318cf76c923cb6ee
dtoriki <dev.toriki@gmail.com>
2022-05-02 21:47:24 +0500

    [WIP] Redesigned.

--- src/EconomyMonitor.Wpf/MVVM/Views/MainWindow.xaml.cs
+++ src/EconomyMonitor.Wpf/MVVM/Views/MainWindow.xaml.cs
@@ -3,9 +3,12 @@ using System.Windows;
 namespace EconomyMonitor.Wpf.MVVM.Views;
 
 /// <summary>
-/// Interaction logic for MainWindow.
+/// Presents application's main window.
 /// </summary>
 public partial class MainWindow : Window
 {
+    /// <summary>
+    /// Creates application main window.
+    /// </summary>
     public MainWindow() => InitializeComponent();
 }
