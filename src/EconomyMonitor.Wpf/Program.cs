5d6241b1ea9d32c9c0b760f8425fb96f20c2dcc8
dtoriki <dev.toriki@gmail.com>
2022-05-08 03:51:16 +0500

    [WIP] Add host builder.

---
+++ src/EconomyMonitor.Wpf/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EconomyMonitor.Wpf;
+
+/// <summary>
+/// Application entry point class.
+/// </summary>
+internal sealed class Program
+{
+    private const string APP_SETTINGS_FILE = "appsettings.json";
+    private const string APP_SETTINGS_DEV_FILE = "appsettings.Development.json";
+
+    private Program() { }
+
+    /// <summary>
+    /// Application entry point.
+    /// </summary>
+    [STAThread]
+    public static void Main()
+    {
+        App app = new();
+        app.InitializeComponent();
+        app.Run();
+    }
+
+    /// <summary>
+    /// Creates, configure and returns host builder.
+    /// </summary>
+    /// <param name="args">CL arguments.</param>
+    /// <returns>Host builder.</returns>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .UseContentRoot(Environment.CurrentDirectory)
+            .ConfigureAppConfiguration((host, config) =>
+            {
+                config
+                    .SetBasePath(Environment.CurrentDirectory)
+                    .AddJsonFile(APP_SETTINGS_FILE);
+
+                if (host.HostingEnvironment.IsDevelopment())
+                {
+                    config.AddJsonFile(APP_SETTINGS_DEV_FILE);
+                }
+
+            });
+}
