52c1be8f0998c0203b83757f5864bfd456655777
dtoriki <dev.toriki@gmail.com>
2022-05-08 13:53:31 +0500

    [WIP] Add period interface.

--- src/EconomyMonitor.Wpf/MVVM/ViewModels/Periods/AddPeriodViewModel.cs
+++ src/EconomyMonitor.Wpf/MVVM/ViewModels/Periods/AddPeriodViewModel.cs
@@ -1,3 +1,4 @@
+using EconomyMonitor.Abstacts;
 using EconomyMonitor.Wpf.MVVM.Abstracts;
 
 namespace EconomyMonitor.Wpf.MVVM.ViewModels.Periods;
@@ -8,33 +9,27 @@ namespace EconomyMonitor.Wpf.MVVM.ViewModels.Periods;
 /// <remarks>
 /// Inherits <see cref="ViewModelBase"/>.
 /// </remarks>
-public class AddPeriodViewModel : ViewModelBase
+public class AddPeriodViewModel : ViewModelBase, IPeriod
 {
     private DateOnly? _startPeriod;
     private DateOnly? _endPeriod;
     private decimal? _income;
 
-    /// <summary>
-    /// Gets or sets period starting date.
-    /// </summary>
+    /// <inheritdoc/>
     public DateOnly StartPeriod
     {
         get => _startPeriod ??= DateOnly.FromDateTime(DateTime.Now);
         set => _ = SetPropertyNotifiable(ref _startPeriod, value);
     }
 
-    /// <summary>
-    /// Gets or sets period ending date.
-    /// </summary>
+    /// <inheritdoc/>
     public DateOnly EndPeriod
     {
         get => _endPeriod ??= DateOnly.FromDateTime(DateTime.Now);
         set => _ = SetPropertyNotifiable(ref _endPeriod, value);
     }
 
-    /// <summary>
-    /// Gets or sets income value for period.
-    /// </summary>
+    /// <inheritdoc/>
     public decimal Income
     {
         get => _income ??= decimal.Zero;
