a818c1862ac70c5cfcd4f656d0f0bd22862f112f
dtoriki <dev.toriki@gmail.com>
2022-05-10 17:00:40 +0500

    [WIP] Added argument null check and cancelation token.

--- src/EconomyMonitor.Services/UnitOfWork/IPeriodsUnitOfWork.cs
+++ src/EconomyMonitor.Services/UnitOfWork/IPeriodsUnitOfWork.cs
@@ -12,7 +12,8 @@ public interface IPeriodsUnitOfWork
     /// </summary>
     /// <typeparam name="TPeriod">Type of <see cref="IPeriod"/>.</typeparam>
     /// <param name="period">Period.</param>
+    /// <param name="cancellationToken">Cancel token.</param>
     /// <returns>Returns back entry period.</returns>
-    Task<IPeriod> CreatePeriodAsync<TPeriod>(TPeriod period)
+    Task<IPeriod> CreatePeriodAsync<TPeriod>(TPeriod period, CancellationToken cancellationToken = default)
         where TPeriod : class, IPeriod;
 }
