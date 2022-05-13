4e50abea436ae0f1ac79ca611a437d93bfd6542c
dtoriki <dev.toriki@gmail.com>
2022-05-13 06:07:44 +0500

    [BUG] Change type of binded UIElement to Base UIElement.

---
+++ src/EconomyMonitor.Wpf/MVVM/Controls/Periods/PeriodsGrid.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows.Controls;
+
+namespace EconomyMonitor.Wpf.MVVM.Controls.Periods;
+/// <summary>
+/// Логика взаимодействия для PeriodsGrid.xaml
+/// </summary>
+public partial class PeriodsGrid : Grid
+{
+    public PeriodsGrid() => InitializeComponent();
+}
