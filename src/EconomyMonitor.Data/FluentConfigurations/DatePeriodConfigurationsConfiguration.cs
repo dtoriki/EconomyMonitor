cc2a5da654ca2747fefa81aba085154e7280ceea
dtoriki <dev.toriki@gmail.com>
2022-06-06 03:28:41 +0500

    (wip) Добавлен доступ к таблице "DatePeriodConfigurations" через контекст БД.
    - Настроены связи между таблицами.
    - Добавлена миграция по добавлению таблицы "DatePeriodConfigurations".

---
+++ src/EconomyMonitor.Data/FluentConfigurations/DatePeriodConfigurationsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using EconomyMonitor.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EconomyMonitor.Data.FluentConfigurations;
+
+internal class DatePeriodConfigurationsConfiguration : IEntityTypeConfiguration<DatePeriodConfigurationEntity>
+{
+    public void Configure(EntityTypeBuilder<DatePeriodConfigurationEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder
+            .HasIndex(i => i.Id)
+            .IsUnique(unique: true);
+
+        builder
+            .HasMany(m => m.PeriodSplits)
+            .WithOne(o => o.DatePeriodOption)
+            .HasForeignKey(k => k.DatePeriodOptionId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .HasMany(m => m.SpendingQuotas)
+            .WithOne(o => o.DatePeriodOption)
+            .HasForeignKey(k => k.DatePeriodOptionId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ src/EconomyMonitor.Migrations/20220605222535_AddDatePeriodConfigurationsTable.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EconomyMonitor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDatePeriodConfigurationsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DatePeriodConfigurations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MonthDayType = table.Column<int>(type: "INTEGER", nullable: false),
+                    MonthDay = table.Column<int>(type: "INTEGER", nullable: true),
+                    IsDefault = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatePeriodConfigurations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PeriodSplitEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MonthDayType = table.Column<int>(type: "INTEGER", nullable: false),
+                    MonthDay = table.Column<int>(type: "INTEGER", nullable: true),
+                    DatePeriodOptionId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PeriodSplitEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PeriodSplitEntity_DatePeriodConfigurations_DatePeriodOptionId",
+                        column: x => x.DatePeriodOptionId,
+                        principalTable: "DatePeriodConfigurations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpendingQuotaEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Quota = table.Column<decimal>(type: "TEXT", nullable: false),
+                    DatePeriodOptionId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpendingQuotaEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SpendingQuotaEntity_DatePeriodConfigurations_DatePeriodOptionId",
+                        column: x => x.DatePeriodOptionId,
+                        principalTable: "DatePeriodConfigurations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DatePeriodConfigurations_Id",
+                table: "DatePeriodConfigurations",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PeriodSplitEntity_DatePeriodOptionId",
+                table: "PeriodSplitEntity",
+                column: "DatePeriodOptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpendingQuotaEntity_DatePeriodOptionId",
+                table: "SpendingQuotaEntity",
+                column: "DatePeriodOptionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PeriodSplitEntity");
+
+            migrationBuilder.DropTable(
+                name: "SpendingQuotaEntity");
+
+            migrationBuilder.DropTable(
+                name: "DatePeriodConfigurations");
+        }
+    }
+}
