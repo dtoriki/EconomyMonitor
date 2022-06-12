0fe7212cf2b3477298dfba31696c79dd35911571
dtoriki <dev.toriki@gmail.com>
2022-06-12 20:34:37 +0500

    (wip) Добавлен метод полчения именованной константы по её описанию.
    - Добавлен статический класс, в котором содержатся методы для работы с именованными константами.

---
+++ src/EconomyMonitor.Helpers/EnumHelper.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using static EconomyMonitor.Helpers.ThrowHelper;
+
+namespace EconomyMonitor.Helpers;
+
+/// <summary>
+/// Содержит методы для работы с <see cref="Enum"/>.
+/// </summary>
+/// <exception cref="InvalidOperationException"/>
+public static class EnumHelper
+{
+    /// <summary>
+    /// Возвращает именованную константу <see cref="Enum"/>,
+    /// по её описанию <see cref="DisplayAttribute.Description"/>.
+    /// </summary>
+    /// <param name="targetType">Тип именованной константы.</param>
+    /// <param name="displayDescription">
+    /// Описание именованной константы <see cref="DisplayAttribute.Description"/>.
+    /// </param>
+    /// <returns>Именованная константа.</returns>
+    /// <exception cref="InvalidOperationException">
+    /// Возникает когда в типе именованных констант, 
+    /// есть больше чем одна именованная константа 
+    /// с одним описанием <see cref="DisplayAttribute.Description"/>.
+    /// </exception>
+    public static Enum? GetEnumValueByDisplay(Type targetType, string displayDescription)
+    {
+        _ = ThrowIfArgumentNull(displayDescription);
+
+        MemberInfo? memberInfo = targetType
+           .GetMembers()
+           .SingleOrDefault(m => m.GetCustomAttribute<DisplayAttribute>()?.Description == displayDescription);
+
+        if (memberInfo is null)
+        {
+            return null;
+        }
+
+        Array values = Enum.GetValues(targetType);
+
+        foreach (object value in values)
+        {
+            if (value.ToString() == memberInfo.Name)
+            {
+                return value as Enum;
+            }    
+        }
+
+        return null;
+    }
+}
