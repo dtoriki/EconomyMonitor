ac80899167d703b6e494d44536447ca276b03394
dtoriki <dev.toriki@gmail.com>
2022-06-12 21:57:31 +0500

    (wip) Добавлена миграция БД.
    - Добавляет поле Percent в таблицу SpendingQuotaEntity.

---
+++ src/EconomyMonitor.Migrations/20220612170004_AddPercentQuota.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EconomyMonitor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPercentQuota : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Percent",
+                table: "SpendingQuotaEntity",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Percent",
+                table: "SpendingQuotaEntity");
+        }
+    }
+}
