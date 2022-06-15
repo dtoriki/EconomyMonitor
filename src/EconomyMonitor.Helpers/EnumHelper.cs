48f54062c02503197d92306b6d6f955597897446
dtoriki <dev.toriki@gmail.com>
2022-06-15 11:13:25 +0500

    (wip) Добавлен метод получения именованной константы по её отображаемому имени.
    - Так же был воспроизведён рефакторинг.

--- src/EconomyMonitor.Helpers/EnumHelper.cs
+++ src/EconomyMonitor.Helpers/EnumHelper.cs
@@ -8,29 +8,72 @@ namespace EconomyMonitor.Helpers;
 /// Содержит методы для работы с <see cref="Enum"/>.
 /// </summary>
 /// <exception cref="InvalidOperationException"/>
+/// <exception cref="ArgumentNullException"/>
 public static class EnumHelper
 {
     /// <summary>
     /// Возвращает именованную константу <see cref="Enum"/>,
-    /// по её описанию <see cref="DisplayAttribute.Description"/>.
+    /// по её отображаемому описанию <see cref="DisplayAttribute.Description"/>.
     /// </summary>
     /// <param name="targetType">Тип именованной константы.</param>
     /// <param name="displayDescription">
-    /// Описание именованной константы <see cref="DisplayAttribute.Description"/>.
+    /// Отображаемое описание именованной константы <see cref="DisplayAttribute.Description"/>.
     /// </param>
     /// <returns>Именованная константа.</returns>
     /// <exception cref="InvalidOperationException">
     /// Возникает когда в типе именованных констант, 
     /// есть больше чем одна именованная константа 
-    /// с одним описанием <see cref="DisplayAttribute.Description"/>.
+    /// с одним отображаемым описанием <see cref="DisplayAttribute.Description"/>.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// Возникает когда <paramref name="targetType"/> или <paramref name="displayName"/>
+    /// оказываются <see langword="null"/>.
     /// </exception>
     public static Enum? GetEnumValueByDisplayDescription(Type targetType, string displayDescription)
     {
+        _ = ThrowIfArgumentNull(targetType);
         _ = ThrowIfArgumentNull(displayDescription);
 
+        return GetEnumValueByDisplayProperty(
+            targetType,
+            displayDescription,
+            d => d?.Description);
+    }
+
+    /// <summary>
+    /// Возвращает именованную константу <see cref="Enum"/>,
+    /// по её отображаемому имени <see cref="DisplayAttribute.Name"/>.
+    /// </summary>
+    /// <param name="targetType">Тип именованной константы.</param>
+    /// <param name="displayName">
+    /// Отображаемое имя именованной константы <see cref="DisplayAttribute.Name"/>.
+    /// </param>
+    /// <returns>Именованная константа.</returns>
+    /// <exception cref="InvalidOperationException">
+    /// Возникает когда в типе именованных констант, 
+    /// есть больше чем одна именованная константа 
+    /// с одним отображаемым именем <see cref="DisplayAttribute.Name"/>.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// Возникает когда <paramref name="targetType"/> или <paramref name="displayName"/>
+    /// оказываются <see langword="null"/>.
+    /// </exception>
+    public static Enum? GetEnumValueByDisplayName(Type targetType, string displayName)
+    {
+        _ = ThrowIfArgumentNull(targetType);
+        _ = ThrowIfArgumentNull(displayName);
+
+        return GetEnumValueByDisplayProperty(
+            targetType,
+            displayName,
+            d => d?.Name);
+    }
+
+    private static Enum? GetEnumValueByDisplayProperty(Type targetType, string displayDescription, Func<DisplayAttribute?, string?> selector)
+    {
         MemberInfo? memberInfo = targetType
-           .GetMembers()
-           .SingleOrDefault(m => m.GetCustomAttribute<DisplayAttribute>()?.Description == displayDescription);
+            .GetMembers()
+            .SingleOrDefault(m => selector(m.GetCustomAttribute<DisplayAttribute>())?.Equals(displayDescription) ?? false);
 
         if (memberInfo is null)
         {
@@ -44,7 +87,7 @@ public static class EnumHelper
             if (value.ToString() == memberInfo.Name)
             {
                 return value as Enum;
-            }    
+            }
         }
 
         return null;
