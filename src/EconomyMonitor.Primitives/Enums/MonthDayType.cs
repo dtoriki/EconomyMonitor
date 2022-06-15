d3ffe760109f2620093e5b4409ae20dc8ca96770
dtoriki <dev.toriki@gmail.com>
2022-06-15 10:33:17 +0500

    (refactoring) Изменение свойства атрибута Display.
    Теперь использует свойство Name вместо Description.

--- src/EconomyMonitor.Primitives/Enums/MonthDayType.cs
+++ src/EconomyMonitor.Primitives/Enums/MonthDayType.cs
@@ -10,30 +10,30 @@ public enum MonthDayType
     /// <summary>
     /// Первый день мксяца.
     /// </summary>
-    [Display(Description = "Первый день месяца")]
+    [Display(Name = "Первый день месяца")]
     FirstDayOfTheMonth = 1,
 
     /// <summary>
     /// Последний день месяца.
     /// </summary>
-    [Display(Description = "Последний день месяца")]
+    [Display(Name = "Последний день месяца")]
     LastDayOfTheMonth = 2,
 
     /// <summary>
     /// Середина месяца (15-е число).
     /// </summary>
-    [Display(Description = "Середина месяца (15-е число)")]
+    [Display(Name = "Середина месяца (15-е число)")]
     MiddleOfTheMonth = 3,
 
     /// <summary>
     /// Другое.
     /// </summary>
-    [Display(Description = "Другое")]
+    [Display(Name = "Другое")]
     Other = 4,
 
     /// <summary>
     /// Неизвестно.
     /// </summary>
-    [Display(Description = "Неизвестно")]
+    [Display(Name = "Неизвестно")]
     Unknown = 0
 }
