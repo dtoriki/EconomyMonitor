7cd8b8b3de8cc3632fd8fc0fa7fc4b328612acc7
dtoriki <dev.toriki@gmail.com>
2022-07-15 21:26:56 +0500

    (wip) Добавлен класс сравнения объектов ISettings.

---
+++ src/EconomyMonitor.Primitives/Equality/SettingsEqualityComparer.cs
@@ -0,0 +1,61 @@
+using System.Diagnostics.CodeAnalysis;
+using EconomyMonitor.Abstacts;
+using static EconomyMonitor.Helpers.ThrowHelper;
+
+namespace EconomyMonitor.Primitives.Comparers;
+
+/// <summary>
+/// Предоставляет методы сравнения экземпляров типа <see cref="ISettings"/>.
+/// </summary>
+/// <remarks>
+/// Реализует <see cref="IEqualityComparer{T}"/>.
+/// </remarks>
+/// <exception cref="ArgumentNullException"/>
+public class SettingsEqualityComparer : IEqualityComparer<ISettings>
+{
+    /// <summary>
+    /// Создаёт экземпляр, предоставляющий методы сравнения экземпляров типа <see cref="ISettings"/>.
+    /// </summary>
+    public SettingsEqualityComparer()
+    {
+
+    }
+
+    /// <summary>
+    /// Определяет равенство двух экземпляров типа <see cref="ISettings"/>.
+    /// </summary>
+    /// <param name="x">Первый экземпляр типа <see cref="ISettings"/>.</param>
+    /// <param name="y">Второй экземпляр типа <see cref="ISettings"/>.</param>
+    /// <returns><see langword="true"/>, если экземпляры равны, иначе - <see langword="false"/>.</returns>
+    public bool Equals(ISettings? x, ISettings? y)
+    {
+        if (ReferenceEquals(x, y))
+        {
+            return true;
+        }
+
+        if (x is null || y is null)
+        {
+            return false;
+        }
+
+        return x.StartingBudget == y.StartingBudget;
+    }
+
+    /// <summary>
+    /// Считает и возвращает хэш-код экземляра <paramref name="obj"/>.
+    /// </summary>
+    /// <param name="obj">Экземпляр типа <see cref="ISettings"/>.</param>
+    /// <returns>Хэш-код экземпляра <paramref name="obj"/>.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Возникает, если <paramref name="obj"/> оказался <see langword="null"/>.
+    /// </exception>
+    public int GetHashCode([DisallowNull] ISettings obj)
+    {
+        _ = ThrowIfArgumentNull(obj);
+
+        return HashCode.Combine(obj.StartingBudget);
+    }
+
+    
+}
