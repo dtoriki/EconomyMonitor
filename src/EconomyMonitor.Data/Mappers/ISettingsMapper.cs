58bbd796062efc972cdbb966958584a2f5378a08
dtoriki <dev.toriki@gmail.com>
2022-07-17 23:39:42 +0500

    (wip) ISettingMapper теперь публичный.

--- src/EconomyMonitor.Data/Mappers/ISettingsMapper.cs
+++ src/EconomyMonitor.Data/Mappers/ISettingsMapper.cs
@@ -4,8 +4,17 @@ using EconomyMonitor.Abstacts;
 
 namespace EconomyMonitor.Data.Mappers;
 
-internal interface ISettingsMapper : IMapper
+/// <summary>
+/// Конвертирует типы <see cref="ISettings"/>.
+/// </summary>
+public interface ISettingsMapper : IMapper
 {
+    /// <summary>
+    /// Конвертирует экземпляр <paramref name="settings"/> в экземпляр типа <typeparamref name="TDestanation"/>.
+    /// </summary>
+    /// <typeparam name="TDestanation">Тип в который конвертируется <paramref name="settings"/>.</typeparam>
+    /// <param name="settings">Экземпляр <see cref="ISettings"/>.</param>
+    /// <returns>Сконвертированный экземпляр типа <typeparamref name="TDestanation"/>.</returns>
     [return: NotNullIfNotNull("settings")]
     TDestanation? Map<TDestanation>(ISettings? settings) where TDestanation : class, ISettings;
 }
