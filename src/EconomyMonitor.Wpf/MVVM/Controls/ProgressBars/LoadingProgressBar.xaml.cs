e196fa320da3b4836dfad8c596386225980f5c2b
dtoriki <dev.toriki@gmail.com>
2022-07-30 17:18:37 +0500

    (refactoring) Удаление ненужного контрола.
    - Удалён прогресс бар.

--- src/EconomyMonitor.Wpf/MVVM/Controls/ProgressBars/LoadingProgressBar.xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows.Controls;
-
-namespace EconomyMonitor.Wpf.MVVM.Controls.ProgressBars;
-
-internal partial class LoadingProgressBar : ProgressBar
-{
-    public LoadingProgressBar() => InitializeComponent();
-}
